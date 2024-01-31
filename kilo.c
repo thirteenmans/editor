7347cbcd70f134fdf3e2213a3e73d98d4ed7af76
Thirteenmans <thirteenmans@gmail.com>
2024-01-31 09:19:29 +0800

    Init.

---
+++ kilo.c
@@ -0,0 +1,45 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void disableRawMode()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() 
+{
+    struct termios raw;
+
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    raw = orig_termios;
+    raw.c_iflag &= ~(IXON);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main()
+{
+    char c;
+
+    enableRawMode();
+
+    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    {
+        if(iscntrl(c))
+        {
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
+
+    return 0;
+}
