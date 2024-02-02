6eb094ce0dbd90160d1b914ed67d078296c2455c
Thirteenmans <thirteenmans@gmail.com>
2024-02-02 09:13:32 +0800

    Support read one line from file.

--- kilo.c
+++ kilo.c
@@ -1,4 +1,8 @@
 /* inlcude */
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -216,16 +220,35 @@ int getWindowSize(int *rows, int *cols)
 }
 
 /* file i/o */
-void editorOpen()
+void editorOpen(char *filename)
 {
-    char *line = "Hello, world!";
-    ssize_t linelen = 13;
+    FILE *fp = fopen(filename, "r");
+    if(!fp)
+    {
+        die("fopen");
+    }
 
-    E.row.size = linelen;
-    E.row.chars = malloc(linelen + 1);
-    memcpy(E.row.chars, line, linelen);
-    E.row.chars[linelen] = '\0';
-    E.numrows = 1;
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    linelen = getline(&line, &linecap, fp);
+    if(linelen != -1)
+    {
+        while(linelen > 0 && (line[linelen - 1] == '\n' ||
+                              line[linelen - 1] == '\r'))
+        {
+            linelen--;
+        }
+
+        E.row.size = linelen;
+        E.row.chars = malloc(linelen + 1);
+        memcpy(E.row.chars, line, linelen);
+        E.row.chars[linelen] = '\0';
+        E.numrows = 1;
+    }
+
+    free(line);
+    fclose(fp);
 }
 
 /* input */
@@ -351,7 +374,7 @@ void editorDrawRows(struct abuf *ab)
     {
         if(y >= E.numrows)
         {
-            if( y == E.screenrows / 3)
+            if(E.numrows == 0 && y == E.screenrows / 3)
             {
                 int padding;
                 char welcome[80];
@@ -427,11 +450,15 @@ void initEditor()
     }
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     enableRawMode();
     initEditor();
-    editorOpen();
+
+    if(argc >= 2)
+    {
+        editorOpen(argv[1]);
+    }
 
     while(1)
     {
